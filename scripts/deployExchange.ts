00d383e3afc39ab1015300e2738fa7423835655f
Grom <k0nstamtinus@gmail.com>
2023-05-08 15:59:58 +0600

    Exchange: - Change settings: unite functions - Correct errors and build - Fill data for deploy

--- scripts/deployExchange.ts
+++ scripts/deployExchange.ts
@@ -1,13 +1,60 @@
-import { toNano } from 'ton-core';
-import { Exchange } from '../wrappers/Exchange';
-import { compile, NetworkProvider } from '@ton-community/blueprint';
+import {toNano, beginCell, Address, Cell} from 'ton-core';
+import {Exchange} from '../wrappers/Exchange';
+import {compile, NetworkProvider} from '@ton-community/blueprint';
 
 export async function run(provider: NetworkProvider) {
-    const exchange = provider.open(Exchange.createFromConfig({}, await compile('Exchange')));
+    const exchange = provider.open(Exchange.createFromConfig({
+        addresses: getAddresses(),
+        supplies: getSupplies(),
+        fees: getFees()
+    }, await compile('Exchange')));
 
     await exchange.sendDeploy(provider.sender(), toNano('0.05'));
 
     await provider.waitForDeploy(exchange.address);
 
     // run methods on `exchange`
+}
+
+function getAddresses() {
+    const tonStorage = "";
+    const betMinter = "";
+    const govMinter = "";
+    const gov = "";
+
+    return beginCell()
+        .storeAddress(Address.parse(tonStorage))
+        .storeAddress(Address.parse(betMinter))
+        .storeAddress(Address.parse(govMinter))
+        .storeAddress(Address.parse(gov))
+        .endCell();
+}
+
+function getSupplies() {
+    return beginCell()
+        .storeCoins(0) // ton
+        .storeCoins(0) // bet
+        .storeCoins(0) // gov
+        .endCell();
+}
+
+function getFees() {
+    return beginCell()
+        .storeRef(getFee(1000)) // bet buy
+        .storeRef(getFee(1000)) // bet sell
+        .storeRef(getFee(1000)) // gov buy
+        .storeRef(getFee(1000)) // gov sell
+        .endCell();
+}
+
+function getFee(value: number): Cell {
+    return beginCell()
+        .storeUint(0, 2)
+        .storeInt(value, 16)          // value
+        .storeInt(0, 16)        // min
+        .storeInt(2_000, 16)    // max
+        .storeUint(1_000, 14)   // step
+        .storeUint(100, 14)     // min step
+        .storeUint(2_000, 14)   // max step
+        .endCell();
 }
--- wrappers/Exchange.ts
+++ wrappers/Exchange.ts
@@ -1,13 +1,22 @@
-import { Address, beginCell, Cell, Contract, contractAddress, ContractProvider, Sender, SendMode } from 'ton-core';
+import {Address, beginCell, Cell, Contract, contractAddress, ContractProvider, Sender, SendMode} from 'ton-core';
 
-export type ExchangeConfig = {};
+export type ExchangeConfig = {
+    addresses: Cell,
+    supplies: Cell,
+    fees: Cell
+};
 
 export function exchangeConfigToCell(config: ExchangeConfig): Cell {
-    return beginCell().endCell();
+    return beginCell()
+        .storeRef(config.addresses)
+        .storeRef(config.supplies)
+        .storeRef(config.fees)
+        .endCell();
 }
 
 export class Exchange implements Contract {
-    constructor(readonly address: Address, readonly init?: { code: Cell; data: Cell }) {}
+    constructor(readonly address: Address, readonly init?: { code: Cell; data: Cell }) {
+    }
 
     static createFromAddress(address: Address) {
         return new Exchange(address);
@@ -15,7 +24,7 @@ export class Exchange implements Contract {
 
     static createFromConfig(config: ExchangeConfig, code: Cell, workchain = 0) {
         const data = exchangeConfigToCell(config);
-        const init = { code, data };
+        const init = {code, data};
         return new Exchange(contractAddress(workchain, init), init);
     }
 
